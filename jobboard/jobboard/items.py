fcfbff9b487b283f0ece6ee76f1ff17dc82c56e9
carolinetm82 <thuymicao@gmail.com>
2020-11-15 13:57:37 +0100

    Mongo project added

---
+++ jobboard/jobboard/items.py
@@ -0,0 +1,14 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class JobboardItem(scrapy.Item):
+    title = scrapy.Field()
+    description = scrapy.Field()
+    link = scrapy.Field()
+    pubDate = scrapy.Field()
+    guid = scrapy.Field()
